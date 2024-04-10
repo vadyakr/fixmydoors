070f56e646b170185d7231e092d5ab72622ff962
vadyakr <razorvadya31@gmail.com>
2024-04-10 17:13:50 +0300

    Updates

---
+++ server/chunks/build/client.manifest.mjs
@@ -0,0 +1,180 @@
+const client_manifest = {
+  "_!~{009}~.js": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "nuxt-icon.MfBulapQ.css",
+    "src": "_!~{009}~.js"
+  },
+  "_BkhG-6Ny.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BkhG-6Ny.js",
+    "name": "nuxt-icon",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "nuxt-icon.MfBulapQ.css"
+    ]
+  },
+  "nuxt-icon.MfBulapQ.css": {
+    "file": "nuxt-icon.MfBulapQ.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "_C-zXMCN4.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C-zXMCN4.js",
+    "name": "vue.f36acd1f",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/icons/location.svg": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DPbhhte0.js",
+    "name": "location",
+    "src": "assets/icons/location.svg",
+    "isDynamicEntry": true,
+    "imports": [
+      "_BkhG-6Ny.js",
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/icons/phone.svg": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CD3NLCI2.js",
+    "name": "phone",
+    "src": "assets/icons/phone.svg",
+    "isDynamicEntry": true,
+    "imports": [
+      "_BkhG-6Ny.js",
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/icons/time.svg": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "By6mHn6R.js",
+    "name": "time",
+    "src": "assets/icons/time.svg",
+    "isDynamicEntry": true,
+    "imports": [
+      "_BkhG-6Ny.js",
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dd5hJXbo.js",
+    "name": "error-404",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js",
+      "_C-zXMCN4.js"
+    ],
+    "css": [
+      "error-404.I9UGCG4-.css"
+    ]
+  },
+  "error-404.I9UGCG4-.css": {
+    "file": "error-404.I9UGCG4-.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "TFQJ1a7s.js",
+    "name": "error-500",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C-zXMCN4.js",
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.CyY_XNPK.css"
+    ]
+  },
+  "error-500.CyY_XNPK.css": {
+    "file": "error-500.CyY_XNPK.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CeHAnd_N.js",
+    "name": "entry",
+    "src": "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.3/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.WMjD-giP.css"
+    ]
+  },
+  "entry.WMjD-giP.css": {
+    "file": "entry.WMjD-giP.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BMi3TRwP.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.11.2_@unocss+reset@0.59.0_floating-vue@5.2.2_unocss@0.59.0_vite@5.2.8/node_modules/nuxt/dist/app/entry.js",
+      "assets/icons/location.svg",
+      "assets/icons/time.svg",
+      "assets/icons/phone.svg",
+      "_BkhG-6Ny.js"
+    ],
+    "css": [
+      "index.NpRAb1lG.css"
+    ]
+  },
+  "index.NpRAb1lG.css": {
+    "file": "index.NpRAb1lG.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
