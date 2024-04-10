d29c5657547d5449977b1704c501b7018ff931d0
vadyakr <razorvadya31@gmail.com>
2024-04-10 17:10:28 +0300

    fix: add preview

---
+++ .output/server/chunks/build/location-BQME8RIF.mjs
@@ -0,0 +1,42 @@
+import { N as NuxtIcon } from './nuxt-icon-DlqTrmoT.mjs';
+import { openBlock, createElementBlock, h, createElementVNode } from 'vue';
+import 'vue/server-renderer';
+import './server.mjs';
+import 'node:http';
+import 'node:https';
+import 'node:zlib';
+import 'node:stream';
+import 'node:buffer';
+import 'node:util';
+import 'node:url';
+import 'node:net';
+import 'node:fs';
+import 'node:path';
+import '../routes/renderer.mjs';
+import 'vue-bundle-renderer/runtime';
+import '../runtime.mjs';
+import 'fs';
+import 'path';
+import 'devalue';
+import '@unhead/ssr';
+import 'unhead';
+import '@unhead/shared';
+import 'vue-router';
+
+const _hoisted_1 = {
+  xmlns: "http://www.w3.org/2000/svg",
+  viewBox: "0 0 48 48"
+};
+const _hoisted_2 = /* @__PURE__ */ createElementVNode("path", { d: "M24 4c-7.73 0-14 6.27-14 14 0 10.5 14 26 14 26s14-15.5 14-26c0-7.73-6.27-14-14-14m0 19c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5" }, null, -1);
+const _hoisted_3 = [
+  _hoisted_2
+];
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, [..._hoisted_3]);
+}
+const LocationSVG = { render() {
+  return h(NuxtIcon, { icon: { render }, name: "location" });
+} };
+
+export { LocationSVG as default, render };
+//# sourceMappingURL=location-BQME8RIF.mjs.map
