1f1d3b8993292ac1a127df47ea194d28cbcc70f0
Vadim <vadim@catigen.com>
2024-06-20 00:27:34 +0300

    fix: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@unocss/nuxt', 'nuxt-svgo', 'nuxt-swiper'],
   css: ['~/styles/main.css'],
+
+  app: {
+    baseURL: 'fixmydoors', // Замените <repository-name> на имя вашего репозитория
+  },
+
+  build: {
+    transpile: ['trpc-nuxt'],
+  },
+
+  nitro: {
+    prerender: {
+      // enabled by default with nuxt generate, not required
+      crawlLinks: false,
+      // https://github.com/nuxt/nuxt/issues/22159
+      failOnError: false,
+    },
+  },
 })
