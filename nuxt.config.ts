0714e3d4e1ec25d3ad4645c2bd4f17299745e077
vadyakr <razorvadya31@gmail.com>
2024-11-07 19:43:19 +0300

    feat: Update site URL in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig({
   },
 
   site: { 
-    url: '', 
+    url: 'https://fixmydoors.by/', 
     name: 'FixMyDoors',
     description: 'Fix My Doors - Ваш надёжный партнёр в ремонте дверей'
     }, 
