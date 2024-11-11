96f0a9116f8ee173238638e1875ee4df3c0859b6
vadyakr <razorvadya31@gmail.com>
2024-11-11 23:27:34 +0300

    feat: Add JivoChat widget script to app.vue on component mount

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
 <template>
   <NuxtPage />
 </template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://code.jivo.ru/widget/GpMnR7dwgM'
+  script.async = true
+  document.head.appendChild(script)
+})
+</script>
